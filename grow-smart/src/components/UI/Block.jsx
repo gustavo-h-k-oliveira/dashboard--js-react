26b14aba9c2917851c93e7558a028454f7e2561a
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2025-02-14 13:56:36 -0300

    Alteração do layout dos cards das bancadas (`Block`).

--- grow-smart/src/components/UI/Block.jsx
+++ grow-smart/src/components/UI/Block.jsx
@@ -15,40 +15,42 @@ export default function Block(props) {
 
     console.log(props)
     return (
-        <div className='block'>
+        <section className='block'>
             <h3>{props.name}</h3>
-            <div className="block-info">
-                <div className="block-info-text">
-                    <h4>Temperatura</h4>
-                    <p>{props.temperature}° C</p>
-                </div>
-                <hr />
-                <div className="block-info-text">
-                    <h4>Umidade do ar</h4>
-                    <p>{props.humidityAir}%</p>
-                </div>
-                <hr />
-                <div className="block-info-text">
-                    <h4>Umidade do solo</h4>
-                    <p>{props.humiditySoil}%</p>
-                </div>
-                <hr />
-                <div className="block-info-text">
-                    <h4>Luminância</h4>
-                    <p>{props.luminance} lux</p>
+            <div className='block-divider'>
+                <article>
+                    <div className="block-info-text">
+                        <h4>Temperatura</h4>
+                        <p>{props.temperature}° C</p>
+                    </div>
+                    <hr />
+                    <div className="block-info-text">
+                        <h4>Umidade do ar</h4>
+                        <p>{props.humidityAir}%</p>
+                    </div>
+                    <hr />
+                    <div className="block-info-text">
+                        <h4>Umidade do solo</h4>
+                        <p>{props.humiditySoil}%</p>
+                    </div>
+                    <hr />
+                    <div className="block-info-text">
+                        <h4>Luminância</h4>
+                        <p>{props.luminance} lux</p>
+                    </div>
+                </article>
+                <div className="block-graph">
+                    <img
+                        src={props.img}
+                        alt="Gráfico de crescimento"
+                    />
+                    <a onClick={handleOpenModal}>Ver detalhes</a>
+                    <Modal show={showModal} onClose={handleCloseModal}>
+                        <h2>Detalhes</h2>
+                        <p>Conteúdo da modal...</p>
+                    </Modal>
                 </div>
             </div>
-            <div className="block-graph">
-                <img
-                    src={props.img}
-                    alt="Gráfico de crescimento"
-                />
-                <a onClick={handleOpenModal}>Ver detalhes</a>
-                <Modal show={showModal} onClose={handleCloseModal}>
-                    <h2>Detalhes</h2>
-                    <p>Conteúdo da modal...</p>
-                </Modal>
-            </div>
-        </div>
+        </section>
     )
 }
