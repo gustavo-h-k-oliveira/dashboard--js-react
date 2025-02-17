c9fa4670e76152100da2621593f621692390df0a
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2025-02-16 23:31:32 -0300

    Adicionando ícone do botão de fechamento do modal e alterando a sua posição.

--- grow-smart/src/components/UI/Modal.jsx
+++ grow-smart/src/components/UI/Modal.jsx
@@ -27,7 +27,11 @@ export default function Modal({ show, onClose, children }) {
     return (
         <div className="modal-overlay" onClick={handleOverlayClick}>
             <div className="modal">
-                <button className="close-button" onClick={onClose}>X</button>
+                <button className="close-button" onClick={onClose}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg>
+                </button>
                 {children}
             </div>
         </div>
